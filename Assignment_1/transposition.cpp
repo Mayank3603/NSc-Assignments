a720e811afa93577d662187cd3165294523f0467
Krishna Somani <108231417+krishna21058@users.noreply.github.com>
2024-02-01 19:32:11 +0530

    completed transposition.cpp

--- Assignment_1/transposition.cpp
+++ Assignment_1/transposition.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 void solve(int index, vector<vector<int>> &ans, vector<int> &nums, vector<int> &temp)
 {
     if (index == nums.size())
@@ -19,6 +20,17 @@ void solve(int index, vector<vector<int>> &ans, vector<int> &nums, vector<int> &
         }
     }
 }
+
+int calc_hash(string s)
+{
+    int hash = s[0];
+    for (int i = 0; i < s.size(); i++)
+    {
+        hash = hash ^ s[i];
+    }
+    return hash;
+}
+
 vector<vector<int>> permute(vector<int> nums)
 {
     vector<vector<int>> ans;
@@ -27,19 +39,19 @@ vector<vector<int>> permute(vector<int> nums)
     return ans;
 }
 
-unsigned int calc_hash(const std::string &input)
+unsigned int jenkins_hash(const string &plaintext)
 {
-    unsigned int hashValue = 0;
-
-    for (char ch : input)
+    unsigned int hash = 0;
+    for (char character : plaintext)
     {
-        hashValue ^= static_cast<unsigned int>(ch);       // XOR operation
-        hashValue = (hashValue << 5) | (hashValue >> 27); // Bitwise shift
-        hashValue += static_cast<unsigned int>(ch);       // Addition
-        hashValue *= 31;                                  // Multiplication
+        hash += static_cast<unsigned int>(character);
+        hash += (hash << 10);
+        hash ^= (hash >> 6);
     }
-
-    return hashValue;
+    hash += (hash << 3);
+    hash ^= (hash >> 11);
+    hash += (hash << 15);
+    return hash;
 }
 
 string encrypt(vector<int> key, const string &plaintext)
@@ -62,31 +74,15 @@ string encrypt(vector<int> key, const string &plaintext)
             matrix[i][j] = plaintext[k++];
         }
     }
-    int val = k % col;
+    int padding = k % col;
 
-    if (val)
+    if (padding)
     {
-        for (; val < col; val++)
+        for (; padding < col; padding++)
         {
-            matrix[row - 1][val] = '#';
+            matrix[row - 1][padding] = 'z';
         }
     }
-    cout << endl;
-    for (int i = 0; i < col; i++)
-    {
-        cout << key[i] << " ";
-    }
-    cout << endl;
-    for (int m = 0; m < row; m++)
-    {
-
-        for (int n = 0; n < col; n++)
-        {
-            cout << matrix[m][n] << " ";
-        }
-        cout << endl;
-    }
-    cout << endl;
     unordered_map<int, int> key_map;
     for (int i = 0; i < key.size(); i++)
     {
@@ -94,7 +90,6 @@ string encrypt(vector<int> key, const string &plaintext)
     }
 
     string ciphertext = "";
-
     for (int j = 1; j <= key.size(); j++)
     {
         for (int i = 0; i < row; i++)
@@ -102,10 +97,7 @@ string encrypt(vector<int> key, const string &plaintext)
             ciphertext += matrix[i][key_map[j]];
         }
     }
-
     return ciphertext;
-
-    // return ciphertext;
 }
 
 string decrypt(vector<int> key, const string &ciphertext)
@@ -120,7 +112,6 @@ string decrypt(vector<int> key, const string &ciphertext)
     {
         key_map[key[i]] = i;
     }
-
     int k = 0;
     for (int j = 1; j <= key.size(); j++)
     {
@@ -129,7 +120,6 @@ string decrypt(vector<int> key, const string &ciphertext)
             matrix[i][key_map[j]] = ciphertext[k++];
         }
     }
-
     string plaintext = "";
 
     for (int i = 0; i < row; i++)
@@ -139,7 +129,6 @@ string decrypt(vector<int> key, const string &ciphertext)
             plaintext += matrix[i][j];
         }
     }
-
     return plaintext;
 }
 string remove_padding(string decrypted_msg)
@@ -147,10 +136,20 @@ string remove_padding(string decrypted_msg)
     int i = 0;
     for (i = decrypted_msg.size() - 1; i >= 0; i--)
     {
-        if (decrypted_msg[i] != '#')
+        if (decrypted_msg[i] != 'z')
             break;
     }
     return decrypted_msg.substr(0, i + 1);
+}
+string remove_zeros(string hash_20bits)
+{
+    int i = 0;
+    for (i = 0; i < hash_20bits.size(); i++)
+    {
+        if (hash_20bits[i] != '0')
+            break;
+    }
+    return hash_20bits.substr(i, hash_20bits.size() - i);
 }
 void print_keyset(const std::vector<std::vector<int>> &keySet)
 {
@@ -164,22 +163,37 @@ void print_keyset(const std::vector<std::vector<int>> &keySet)
     }
     cout << endl;
 }
-bool check_all_ciphertexts(vector<int> transposition_key, vector<string> ciphertexts_vec, vector<string> plaintexts_vec)
+bool check_all_ciphertexts(vector<int> transposition_key, vector<string> ciphertexts_vec)
 {
-    for (int i = 1; i < plaintexts_vec.size(); i++)
+    map<char, char> Alp_Num = {{'a', '0'}, {'b', '1'}, {'c', '2'}, {'d', '3'}, {'e', '4'}, {'f', '5'}, {'g', '6'}, {'h', '7'}, {'i', '8'}, {'j', '9'}};
+    for (int i = 1; i < ciphertexts_vec.size(); i++)
     {
         string decrypted_msg = decrypt(transposition_key, ciphertexts_vec[i]);
         string final_decrypted = remove_padding(decrypted_msg);
-        cout << "Decrypted:" << final_decrypted << "\n"
-             << "Decrypted hash:" << calc_hash(final_decrypted) << " " << endl;
-        if (calc_hash(final_decrypted) != calc_hash(plaintexts_vec[i]))
+
+        if (final_decrypted.size() <= 20)
+        {
             return false;
+        }
+        string decrypted_without_hash = final_decrypted.substr(0, final_decrypted.size() - 20);
+        string hash_value_alp = final_decrypted.substr(final_decrypted.size() - 20);
+        string hash_value_num = "";
+        for (auto it : hash_value_alp)
+        {
+            hash_value_num += Alp_Num[it];
+        }
+        // cout << final_decrypted << " " << decrypted_without_hash << " " << remove_zeros(hash_value_num) << endl;
+        if (to_string(jenkins_hash(decrypted_without_hash)) != remove_zeros(hash_value_num))
+        {
+            return false;
+        }
     }
     return true;
 }
-vector<int> brute_force(vector<string> ciphertexts_vec, vector<string> plaintexts_vec)
+vector<int> brute_force(vector<string> ciphertexts_vec)
 {
     vector<int> discover_key = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    map<char, char> Alp_Num = {{'a', '0'}, {'b', '1'}, {'c', '2'}, {'d', '3'}, {'e', '4'}, {'f', '5'}, {'g', '6'}, {'h', '7'}, {'i', '8'}, {'j', '9'}};
     for (int i = 1; i < 10; i++)
     {
         if (ciphertexts_vec[0].size() % i != 0)
@@ -187,16 +201,30 @@ vector<int> brute_force(vector<string> ciphertexts_vec, vector<string> plaintext
         vector<int> key_elements(discover_key.begin(), discover_key.begin() + i);
         vector<vector<int>> key_set = permute(key_elements);
 
+        // print_keyset(key_set);
         for (auto transposition_key : key_set)
         {
             string decrypted_msg = decrypt(transposition_key, ciphertexts_vec[0]);
             string final_decrypted = remove_padding(decrypted_msg);
 
-            if (calc_hash(final_decrypted) != calc_hash(plaintexts_vec[0]))
+            if (final_decrypted.size() <= 20)
+            {
+                continue;
+            }
+            string decrypted_without_hash = final_decrypted.substr(0, final_decrypted.size() - 20);
+            string hash_value_alp = final_decrypted.substr(final_decrypted.size() - 20);
+            string hash_value_num = "";
+            for (auto it : hash_value_alp)
+            {
+                hash_value_num += Alp_Num[it];
+            }
+
+            if (to_string(jenkins_hash(decrypted_without_hash)) != remove_zeros(hash_value_num))
                 continue;
             else
             {
-                if (check_all_ciphertexts(transposition_key, ciphertexts_vec, plaintexts_vec) == true)
+                // cout<<final_decrypted << " " << decrypted_without_hash << " " << remove_zeros(hash_value_num) << endl;
+                if (check_all_ciphertexts(transposition_key, ciphertexts_vec) == true)
                     return transposition_key;
             }
         }
@@ -204,38 +232,65 @@ vector<int> brute_force(vector<string> ciphertexts_vec, vector<string> plaintext
     cout << " KEY NOT FOUND" << endl;
     return {-1};
 }
+string plain_hash_concatenation(const string &plaintext)
+{
+    map<int, char> Num_Alp;
+    for (int i = 0; i <= 9; ++i)
+    {
+        Num_Alp[i] = 'a' + i;
+    }
+
+    unsigned int hash_value = jenkins_hash(plaintext);
+
+    string hash_converted = "";
+
+    // Ensuring the hash value has 20 digits
+    string hash_str = to_string(hash_value);
+    hash_str = string(20 - hash_str.length(), '0') + hash_str; // Pad with zeros to the left
+
+    for (char digit : hash_str)
+    {
+        hash_converted += Num_Alp[digit - '0'];
+    }
+    cout << "Plaintext: " << plaintext<<endl; 
+    cout << "Hash: " << hash_converted << endl;
+    cout << "New Plaintext: " << plaintext + hash_converted << endl;
+
+    return plaintext + hash_converted;
+}
 int main()
 {
     vector<int> key = {3, 1, 4, 2, 5, 6, 7};
-    // string plaintext = "helloworld";
 
     vector<string> plaintexts_vec;
+
     for (int i = 0; i < 5; i++)
     {
         string plaintext;
+        cout << "Enter Plaintext_" << i + 1 << ": ";
         cin >> plaintext;
-        plaintexts_vec.push_back(plaintext);
+        plaintexts_vec.push_back(plain_hash_concatenation(plaintext));
+        cout<<endl;
     }
-    for (auto plaintext : plaintexts_vec)
-        cout << "Plaintext: " << plaintext << "\n"
-             << "Plaintext Hash: " << calc_hash(plaintext) << endl;
 
     vector<string> ciphertexts_vec;
-    vector<string> decrypted_vec;
     for (auto plaintext : plaintexts_vec)
     {
         ciphertexts_vec.push_back(encrypt(key, plaintext));
     }
-    for (auto ciphertext : ciphertexts_vec)
-    {
-        cout << "Ciphertext: " << ciphertext << endl;
-    }
-    for (auto ciphertext : ciphertexts_vec)
-    {
-        decrypted_vec.push_back(decrypt(key, ciphertext));
-    }
 
-    vector<int> discovered_key = brute_force(ciphertexts_vec, plaintexts_vec);
+    //creating ciphertexts including padding with "z"
+    for (int i = 0; i < plaintexts_vec.size(); i++)
+    {
+        cout << "Ciphertext_"<<i+1<<": " << encrypt(key, plaintexts_vec[i]) << endl;
+    }
+    cout<<endl;
+
+    cout<<endl;
+    cout<<"---- Applying BruteForce to Discover Key ----"<<endl;
+    cout<<endl;
+
+    vector<int> discovered_key = brute_force(ciphertexts_vec);
     cout << "Discovered key: ";
     for (auto it : discovered_key)
     {
